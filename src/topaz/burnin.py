9971ab5ddc5ac72a03ee8d56a8dc5325d0b32f6c
boqiling <boqiling@gmail.com>
2013-12-20 23:23:34 +0800

    update burnin.py

--- src/topaz/burnin.py
+++ src/topaz/burnin.py
@@ -3,47 +3,97 @@
 
 from i2c_adapter.i2c_adapter import DeviceAPI
 
+REG_MAP = [{"name": "READINESS", "addr": 0x04},
+           {"name": "PGEMSTAT",  "addr": 0x05},
+           {"name": "TEMP",      "addr": 0x06},
+           {"name": "VIN",       "addr": 0x07},
+           {"name": "VCAP",      "addr": 0x08},
+           {"name": "VC1",       "addr": 0x09},
+           {"name": "VC2",       "addr": 0x0A},
+           {"name": "VC3",       "addr": 0x0B},
+           {"name": "VC4",       "addr": 0x0C},
+           {"name": "VC5",       "addr": 0x0D},
+           {"name": "VC6",       "addr": 0x0E},
+           {"name": "RESERVED",  "addr": 0x0F}]
+
+EEP_MAP = [{"name": "PWRCYCS", "addr": 0x0268, "length": 2, "type": "word"},
+           {"name": "LASTCAP", "addr": 0x026A, "length": 1, "type": "int"},
+           {"name": "MODEL",   "addr": 0x026B, "length": 8, "type": "str"},
+           {"name": "FWVER",   "addr": 0x0273, "length": 8, "type": "str"},
+           {"name": "HWVER",   "addr": 0x027B, "length": 8, "type": "str"},
+           {"name": "CAPPN",   "addr": 0x0283, "length": 8, "type": "str"},
+           {"name": "SN",      "addr": 0x028B, "length": 8, "type": "str"},
+           {"name": "PCBVER",  "addr": 0x0293, "length": 8, "type": "str"},
+           {"name": "MFDATE",  "addr": 0x029B, "length": 8, "type": "str"},
+           {"name": "ENDUSR",  "addr": 0x02A3, "length": 8, "type": "str"},
+           {"name": "PCA",     "addr": 0x02AB, "length": 8, "type": "str"},
+           {"name": "CINT",    "addr": 0x02B3, "length": 1, "type": "int"}]
+
+
+def query_map(mymap, **kvargs):
+    """method to search the map (the list of dict, [{}, {}])
+    params: mymap:  the map to search
+            kvargs: query conditon key=value, key should be in the dict.
+    return: the dict match the query contdtion or None.
+    """
+    for row in mymap:
+        for k, v in kvargs.items():
+            if(row[k] != v):
+                break
+            else:
+                return row
+    return None
+
+
 EEPROM_REG_ADDRL = 0
 EEPROM_REG_ADDRH = 1
 EEPROM_REG_RWDATA = 2
 
 
 def read_ee(device, addr):
+    """method to read eeprom data,
+    first write low address byte to register EEPROM_REG_ADDRL,
+    then write high address byte to register EEPROM_REG_ADDRH,
+    then read the data from register EEPROM_REG_RWDATA.
+    params: device: I2C adapter device handle
+            addr:   eeprom address to read
+    return: value of addr in byte
+    """
     device.write_reg(EEPROM_REG_ADDRL, addr & 0xFF)
     device.write_reg(EEPROM_REG_ADDRH, (addr >> 8) & 0xFF)
     val = device.read_reg(EEPROM_REG_RWDATA)
     return val
 
 
-def read_vpd(device, start_addr, bytenum):
-    return [read_ee(device, addr) for addr in range(start_addr, bytenum)]
-
-
-def get_PWRCYCS(device):
-    data1 = read_ee(device, 0x268)
-    data2 = read_ee(device, 0x269)
-    val = data1 + (data2 << 8)
-    return val
-
-
-def get_PWRCYCS2(device):
-    result = read_vpd(device, 0x268, 2)
-    val = result[0] + (result[1] << 8)
-    return val
-
-
-def get_MODEL(device):
-    read_vpd(device, 0x026B, 8)
+def read_vpd(device, eep_name):
+    """method to read eep_data according to eep_name
+    eep is one dict in eep_map, for example:
+    {"name": "CINT", "addr": 0x02B3, "length": 1, "type": "int"}
+    """
+    eep = query_map(name=eep_name)      # eep is one dict in eep_map
+    start = eep["addr"]                 # start_address
+    length = eep["lenght"]              # length
+    typ = eep["type"]                   # type
+    datas = [read_ee(device, addr) for addr in range(start, (start + length))]
+    if(typ == "word"):
+        val = datas[0] + (datas[1] << 8)
+        return val
+    if(typ == "str"):
+        return ''.join(chr(i) for i in datas)
+    if(typ == "int"):
+        return datas[0]
 
 
 if __name__ == "__main__":
+    vcap = query_map(REG_MAP, name="VCAP")
+    sn = query_map(EEP_MAP, name="SN")
+
     da = DeviceAPI(bitrate=100)
     da.open(portnum=0)
     da.slave_addr = 20
     while 1:
-        print "PGEM: " + str(da.read_reg(0x05))
-        print "TEMP: " + str(da.read_reg(0x06))
-        print "VCAP: " + str(da.read_reg(0x08))
-        print "Power Cycle: " + str(get_PWRCYCS(da))
-        print "Power Cycle2: " + str(get_PWRCYCS2(da))
+        print "MODEL" + ": " + read_vpd(da, "MODEL")
+        print "SN   " + ": " + read_vpd(da, "SN")
+        #print vcap["name"] + str(da.read_reg(vcap["addr"]))
+        #print sn["name"] + read_EEPROM(da, sn["addr"], sn["length"])
         da.sleep(1000)
