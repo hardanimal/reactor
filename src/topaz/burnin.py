e533eb49e5fa60360a5951aaed48844063aca463
boqiling <boqiling@gmail.com>
2014-05-05 11:21:23 +0800

    add delay time for each channel

--- src/topaz/burnin.py
+++ src/topaz/burnin.py
@@ -27,6 +27,8 @@ def main():
         f.run()
         channel_list.append(f)
 
+    wait_for_discharge = 0
+
     burnin_finish = False
     logging.info("====================burnin start=========================")
     while(not burnin_finish):
@@ -35,12 +37,13 @@ def main():
             if(f.status.value == ChannelStates.EXIT):
                 # check if already finished.
                 burnin_finish &= True
-                logging.info("--------------channel finish.----------------------")
+                logging.info("----------channel finish.------------------")
+                wait_for_discharge += 30
                 continue
             else:
                 burnin_finish &= False
-                logging.info("--------------channel not finish.----------------------")
-            logging.info("--------------channel start----------------------")
+                logging.info("----------channel not finish.------------------")
+            logging.info("----------channel start------------------")
             # start one cycle
             f.en_queue(ChannelStates.run)
             time.sleep(1)
@@ -51,6 +54,8 @@ def main():
                 # check if the channle has finished burnin
                 time.sleep(5)
             logging.info("--------------channel done.----------------------")
+        # used to prevent only one channel left and get never finished.
+        time.sleep(wait_for_discharge)
     logging.info("====================burnin done.=========================")
 
 
